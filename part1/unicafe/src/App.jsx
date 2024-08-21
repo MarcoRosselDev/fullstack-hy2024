20a8349889d71f683b4571170babe7175b3baba2
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-21 14:17:42 -0400

    part 1 | exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,52 +1,50 @@
 import { useState } from 'react'
 import "./style.css"
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, bad, neutral}) => {
+  const total = bad + neutral + good;
+  const positive =  good/total * 100;
+  const average = (good - bad) / total;
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good"  value={good} />
+        <StatisticLine text="neutral"  value={neutral} />
+        <StatisticLine text="bad"  value={bad} />
+        <StatisticLine text="all"  value={total} />
+        <StatisticLine text="average"  value={average? average.toFixed(2): 0} />
+        <StatisticLine text="positive"  value={positive? positive.toFixed(2): 0} />
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const positive =  good/(bad + neutral + good) * 100;
-  const average = (good - bad) / (good + bad + neutral);
-
   return (
     <>
-      <h1>give feedback</h1>
-      <div>
-      <button onClick={() => setGood(prev => prev + 1)}>good</button>
-      <button onClick={() => setNeutral(prev => prev + 1)}>neutral</button>
-      <button onClick={() => setBad(prev => prev + 1)}>bad</button>
+      <h2>give feedback</h2>
+      <div id='btns'>
+        <button onClick={() => setGood(prev => prev + 1)}>good</button>
+        <button onClick={() => setNeutral(prev => prev + 1)}>neutral</button>
+        <button onClick={() => setBad(prev => prev + 1)}>bad</button>
       </div>
-      <h1>statistics</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td>good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>all</td>
-            <td>{bad + neutral + good}</td>
-          </tr>
-          <tr>
-            <td>average</td>
-            <td>{average.toFixed(2)}</td>
-          </tr>
-          <tr>
-            <td>positive</td>
-            <td>{positive.toFixed(2)} %</td>
-          </tr>
-        </tbody>
-      </table>
+      <h3>statistics</h3>
+      {good || neutral || bad? <Statistics good={good} neutral={neutral} bad={bad}/> : <p>No feedback given</p>}
+      
     </>
   )
 }
