ac656f9e614c016824acc63972994c642c003990
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-25 11:26:07 -0400

    part 2 | exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
+import axios from "axios";
 
 const App = () => {
   // persons array for testing
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then((response) =>{
+      // manejar respuesta exitosa
+      setPersons(response.data)
+    })
+    .catch((error) => {
+      // manejar error
+      console.log(error);
+    })},[])
+
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
