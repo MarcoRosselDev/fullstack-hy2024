79e8844b10c53026101a5e8b2c5a1091d6325910
Marco Rossel <andresmarcorossel@gmail.com>
2024-08-26 17:49:26 -0400

    part 2 | exercise 2.14 | problem solved

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -29,7 +29,10 @@ const App = () =>{
     .addPhone({name, number})
     .then(data =>{
       console.log(data, 'from create a new phone number');
+      setShow(prev => prev.concat(data))
       setPersons(prev => prev.concat(data))
+      setName(prev => "")
+      setNumber(prev => "")
     })
   }
   const deleteNumber = id => {
@@ -40,21 +43,31 @@ const App = () =>{
     })
   }
   const handleSearch = event => {
-    console.log(event.target.value)
     setShow(persons.filter(item => item.name.toLowerCase().includes(event.target.value.toLowerCase()) ? item : null))
   }
-
   return (
     <div>
       serch: <input type="text" onChange={handleSearch} />
       <h2>new number</h2>
       <form action="" onSubmit={addNumber}>
-        <input type="text" onChange={handleName} /><br/>
-        <input type="text" onChange={handleNumber} /><br/>
+        <input type="text" onChange={handleName} value={name} /><br/>
+        <input type="text" onChange={handleNumber} value={number} /><br/>
         <button type="submit">add</button>
       </form>
-      {}
-      {show.map(item => <div key={item.id}>{item.name} {item.number}<button onClick={() => deleteNumber(item.id)}>delete</button></div> )}
+      {persons.map(item => {
+        if (show.find(i => i.id === item.id)){
+          return (
+            <div key={item.id}>
+              {item.name} {item.number}<button onClick={() => deleteNumber(item.id)}>delete</button>
+            </div>)
+        } else {
+          return
+        }
+        return (
+        <div key={item.id}>
+          {item.name} {item.number}<button onClick={() => deleteNumber(item.id)}>delete</button>
+        </div>)
+      })}
     </div>
   )
 }
