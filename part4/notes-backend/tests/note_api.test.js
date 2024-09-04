abd4a3890862e32c7c95ee2cefae8fe036ffd62c
Marco Rossel <andresmarcorossel@gmail.com>
2024-09-03 23:36:25 -0400

    part4 | notes tests successful

--- part4/notes-backend/tests/note_api.test.js
+++ part4/notes-backend/tests/note_api.test.js
@@ -4,22 +4,11 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
+const { initialNotes, notesInDb } = require('./test_helper')
 
 const api = supertest(app)
-
 // antes de iniciar borramos toda la base de datos de prueba
 // luego publicamos este array en la base de datos de prueba
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-]
-
 beforeEach(async () => {
   await Note.deleteMany({})
   let noteObject = new Note(initialNotes[0])
@@ -46,6 +35,47 @@ test('the first note is about HTTP methods', async () => {
 
   const contents = response.body.map((e) => e.content)
   assert.strictEqual(contents.includes('HTML is easy'), true)
+})
+
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  /* const response = await api.get('/api/notes')
+
+  const contents = response.body.map((r) => r.content)
+
+  assert.strictEqual(response.body.length, initialNotes.length + 1) */
+  const notesAtEnd = await notesInDb()
+  assert.strictEqual(notesAtEnd.length, initialNotes.length + 1)
+
+  const contents = notesAtEnd.map((n) => n.content)
+
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true,
+  }
+  await api.post('/api/notes').send(newNote).expect(400)
+  const notesAtEnd = await notesInDb()
+
+  assert.strictEqual(notesAtEnd.length, initialNotes.length)
+
+  /* await api.post('/api/notes').send(newNote).expect(400)
+
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length) */
 })
 
 after(async () => {
