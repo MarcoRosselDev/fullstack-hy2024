ed0ad314b65056557015a58e1b4d863997928632
Marco Rossel <andresmarcorossel@gmail.com>
2024-09-05 23:31:01 -0400

    part4 | testing notes and hash passwords

--- part4/notes-backend/tests/note_api.test.js
+++ part4/notes-backend/tests/note_api.test.js
@@ -1,130 +1,152 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
+const bcrypt = require('bcrypt')
 const Note = require('../models/note')
+const User = require('../models/user')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
-const { initialNotes, notesInDb } = require('./test_helper')
-
+const { initialNotes, notesInDb, usersInDb } = require('./test_helper')
 const api = supertest(app)
-// antes de iniciar borramos toda la base de datos de prueba
-// luego publicamos este array en la base de datos de prueba
-/* beforeEach(async () => {
-  await Note.deleteMany({})
-  let noteObject = new Note(initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(initialNotes[1])
-  await noteObject.save()
-}) */
-beforeEach(async () => {
-  await Note.deleteMany({})
 
-  const noteObjects = initialNotes.map((note) => new Note(note))
-  const promiseArray = noteObjects.map((note) => note.save())
-  await Promise.all(promiseArray)
+describe('when there as initially some notes saved', () => {
+  beforeEach(async () => {
+    await Note.deleteMany({})
+    const noteObjects = initialNotes.map((note) => new Note(note))
+    const promiseArray = noteObjects.map((note) => note.save())
+    await Promise.all(promiseArray)
+  })
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are two notes', async () => {
+    const response = await api.get('/api/notes')
+
+    assert.strictEqual(response.body.length, 2)
+  })
+  test('the first note is about HTTP methods', async () => {
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map((e) => e.content)
+    assert.strictEqual(contents.includes('HTML is easy'), true)
+  })
+  test('a specific note can be viewed', async () => {
+    const notesAtStart = await notesInDb()
+
+    const noteToView = notesAtStart[0]
+
+    const resultNote = await api
+      .get(`/api/notes/${noteToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.deepStrictEqual(resultNote.body, noteToView)
+  })
 })
 
-/* si lo que queremos es que se ejecute en orden aplicamos el for of
-por que Promise.all() espera a que cada uno termine pero estan en su propio runtime
-por lo que el orden de termino es aleatorio e inesperado
-beforeEach(async () => {
-  await Note.deleteMany({})
+describe('addition of a new note', () => {
+  test('note without content is not added', async () => {
+    const newNote = {
+      important: true,
+    }
+    await api.post('/api/notes').send(newNote).expect(400)
+    const notesAtEnd = await notesInDb()
 
-  for (let note of helper.initialNotes) {
-    let noteObject = new Note(note)
-    await noteObject.save()
-  }
-})
-*/
-// ahora podemos hacer las pruebas
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    assert.strictEqual(notesAtEnd.length, initialNotes.length)
+  })
+  test('a valid note can be added ', async () => {
+    const newNote = {
+      content: 'async/await simplifies making async calls',
+      important: true,
+    }
 
-test('there are two notes', async () => {
-  const response = await api.get('/api/notes')
+    await api
+      .post('/api/notes')
+      .send(newNote)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  assert.strictEqual(response.body.length, 2)
-})
+    const notesAtEnd = await notesInDb()
+    assert.strictEqual(notesAtEnd.length, initialNotes.length + 1)
 
-test('the first note is about HTTP methods', async () => {
-  const response = await api.get('/api/notes')
+    const contents = notesAtEnd.map((n) => n.content)
 
-  const contents = response.body.map((e) => e.content)
-  assert.strictEqual(contents.includes('HTML is easy'), true)
+    assert(contents.includes('async/await simplifies making async calls'))
+  })
 })
 
-test('a valid note can be added ', async () => {
-  const newNote = {
-    content: 'async/await simplifies making async calls',
-    important: true,
-  }
+describe('deletion of a note', () => {
+  test('a note can be deleted', async () => {
+    const notesAtStart = await notesInDb()
+    const noteToDelete = notesAtStart[0]
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api.delete(`/api/notes/${noteToDelete.id}`).expect(204)
 
-  /* const response = await api.get('/api/notes')
+    const notesAtEnd = await notesInDb()
 
-  const contents = response.body.map((r) => r.content)
+    const contents = notesAtEnd.map((r) => r.content)
+    assert(!contents.includes(noteToDelete.content))
 
-  assert.strictEqual(response.body.length, initialNotes.length + 1) */
-  const notesAtEnd = await notesInDb()
-  assert.strictEqual(notesAtEnd.length, initialNotes.length + 1)
-
-  const contents = notesAtEnd.map((n) => n.content)
-
-  assert(contents.includes('async/await simplifies making async calls'))
+    assert.strictEqual(notesAtEnd.length, notesAtStart.length - 1)
+  })
 })
 
-test('note without content is not added', async () => {
-  const newNote = {
-    important: true,
-  }
-  await api.post('/api/notes').send(newNote).expect(400)
-  const notesAtEnd = await notesInDb()
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
 
-  assert.strictEqual(notesAtEnd.length, initialNotes.length)
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
 
-  /* await api.post('/api/notes').send(newNote).expect(400)
+    await user.save()
+  })
 
-  const response = await api.get('/api/notes')
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await usersInDb()
 
-  assert.strictEqual(response.body.length, initialNotes.length) */
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
 })
-
-test('a specific note can be viewed', async () => {
-  const notesAtStart = await notesInDb()
-
-  const noteToView = notesAtStart[0]
-
-  const resultNote = await api
-    .get(`/api/notes/${noteToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  assert.deepStrictEqual(resultNote.body, noteToView)
-})
-
-test('a note can be deleted', async () => {
-  const notesAtStart = await notesInDb()
-  const noteToDelete = notesAtStart[0]
-
-  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204)
-
-  const notesAtEnd = await notesInDb()
-
-  const contents = notesAtEnd.map((r) => r.content)
-  assert(!contents.includes(noteToDelete.content))
-
-  assert.strictEqual(notesAtEnd.length, initialNotes.length - 1)
-})
-
 after(async () => {
   await mongoose.connection.close()
 })
